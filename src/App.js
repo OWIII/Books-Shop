087d0525a3743fc6d4d44836e8258b265f2ecc74
Lusnikov Artem <lusnikov.av@esphere.ru>
2020-07-22 02:57:30 +0300

    Create project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        Привет
+      </div>
+    );
+  }
+}
+
+export default App;
